17e4e10b3044874496994ba35bbffb032b2afc06
Nathan Lin <nlin@athenian.org>
2021-11-03 09:04:55 -0700

    first draft

--- src/main/java/NumberSequence.java
+++ src/main/java/NumberSequence.java
@@ -6,7 +6,7 @@ public class NumberSequence {
     // given startValue and counting up by 1
     // e.g. new NumberSequence(5, 10) should create a NumberSequence whose
     // array arr contains: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    public NumberSequence(int start, int count) {
+    public NumberSequence(int start, int size) {
         // YOUR CODE HERE
     }
 
